925997d6a76e81b5d266d32f3bca514476b36736
kingmusicbotc <vk9163363@gmail.com>
2025-06-17 10:58:20 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,36 +1,43 @@
-# db.py
-
 from pymongo import MongoClient
 import os
 
 # === MongoDB Connection ===
-# You can set MONGO_URI as an environment variable on Render or Replit
-MONGO_URI = os.getenv("MONGO_URI") or "mongodb+srv://grovvewm:grovvewm@cluster0.5aeo13r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = os.getenv("MONGO_URI") or "mongodb+srvvewm:grovvewm@cluster0.5aeo13r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(MONGO_URI)
 db = client["dragon_dusk"]
 
-# === Mapping JSON Filenames to Mongo Collections ===
+# === Mapping JSON Filenames to MongoDB Collections ===
 collection_map = {
     "users.json": db["users"],
     "group.json": db["groups"],
-    "clans.json": db["clans"]
+    "clans.json": db["clans"],
+    "battle.json": db["battles"],
+    "cooldowns.json": db["cooldowns"],
+    "eggs.json": db["eggs"]
 }
 
-# === Load JSON-style data from MongoDB ===
+# === Load entire collection into dictionary ===
 def load_json(filename):
     col = collection_map.get(filename)
     data = {}
-    for doc in col.find():
-        doc_id = str(doc["_id"])
-        doc.pop("_id", None)
-        data[doc_id] = doc
+    if col:
+        for doc in col.find():
+            doc_id = str(doc["_id"])
+            doc.pop("_id", None)
+            data[doc_id] = doc
     return data
 
-# === Save JSON-style data to MongoDB ===
+# === Save entire dictionary to collection ===
 def save_json(filename, data):
     col = collection_map.get(filename)
-    col.delete_many({})  # Clear all existing entries
-    for key, doc in data.items():
-        doc["_id"] = key  # Use key as MongoDB document ID
-        col.insert_one(doc)
+    if col:
+        col.delete_many({})
+        for key, value in data.items():
+            value["_id"] = key
+            col.insert_one(value)
+
+# === Update a single user document (for users.json only) ===
+def update_user(user_id, user_data):
+    users_col = db["users"]
+    users_col.update_one({"_id": str(user_id)}, {"$set": user_data}, upsert=True)
